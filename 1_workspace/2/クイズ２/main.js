43ae9a676c250dd20c5646723d918a7a2eeb721f
taku0708 <78.tk.nghr@gmail.com>
2021-04-20 22:39:11 +0900

    fourQuiz

--- 1_workspace/2/クイズ２/main.js
+++ 1_workspace/2/クイズ２/main.js
@@ -1,10 +1,57 @@
-// 自分で作る問題
+// // 自分で作る問題
+
+// const quizText = document.querySelector("#quiz-text")
+// const quizImage = document.querySelector("#quiz-image")
+// const choice1 = document.querySelector("#choice-1")
+// const choice2 = document.querySelector("#choice-2")
+// const choice3 = document.querySelector("#choice-3")
+// const feedback = document.querySelector("#feedback")
+
+// const quiz = {
+//   text: "ムズムズ問題",
+//   image: "Two.jpeg",
+//   content: [
+//     {
+//       button: "namaste",
+//       feedback: "attya",
+//     },
+//     {
+//       button: "konnitiha",
+//       feedback: "nahin",
+//     },
+//     {
+//       button: "sayounara",
+//       feedback: "nahin",
+//     },
+//   ],
+// }
+
+// const reloadQuiz = () => {
+//   quizText.textContent = "Q" + quiz.text
+//   quizImage.src = "./images/" + quiz.image
+//   choice1.textContent = quiz.content[0].button
+//   choice2.textContent = quiz.content[1].button
+//   choice3.textContent = quiz.content[2].button
+// }
+
+// reloadQuiz()
+
+// const choose = (choiceNumber) => {
+//   feedback.textContent = quiz.content[choiceNumber].feedback
+// }
+
+// choice1.onclick = () => choose(0)
+// choice2.onclick = () => choose(1)
+// choice3.onclick = () => choose(2)
+
+// 4択問題
 
 const quizText = document.querySelector("#quiz-text")
 const quizImage = document.querySelector("#quiz-image")
 const choice1 = document.querySelector("#choice-1")
 const choice2 = document.querySelector("#choice-2")
 const choice3 = document.querySelector("#choice-3")
+const choice4 = document.querySelector("#choice-4")
 const feedback = document.querySelector("#feedback")
 
 const quiz = {
@@ -23,6 +70,10 @@ const quiz = {
       button: "sayounara",
       feedback: "nahin",
     },
+    {
+      button: "yonmonme",
+      feedback: "nahin",
+    },
   ],
 }
 
@@ -32,6 +83,7 @@ const reloadQuiz = () => {
   choice1.textContent = quiz.content[0].button
   choice2.textContent = quiz.content[1].button
   choice3.textContent = quiz.content[2].button
+  choice4.textContent = quiz.content[3].button
 }
 
 reloadQuiz()
@@ -43,3 +95,4 @@ const choose = (choiceNumber) => {
 choice1.onclick = () => choose(0)
 choice2.onclick = () => choose(1)
 choice3.onclick = () => choose(2)
+choice4.onclick = () => choose(3)

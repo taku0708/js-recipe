6fbbc3b359bae91fe23611655466d59faf80875f
taku0708 <78.tk.nghr@gmail.com>
2021-04-24 16:49:05 +0900

    ex2 trello

--- 1_workspace/2/トレロ２/main.js
+++ 1_workspace/2/トレロ２/main.js
@@ -1,6 +1,9 @@
 const inputElement = document.querySelector("#input-todo")
+const inputElement2 = document.querySelector("#input-todo2")
 const container = document.querySelector("#cards-container")
+const container2 = document.querySelector("#cards-container2")
 const addButton = document.querySelector("#add-button")
+const addButton2 = document.querySelector("#add-button2")
 
 addButton.onclick = function() {
   const card = createCard(inputElement.value)
@@ -8,6 +11,14 @@ addButton.onclick = function() {
 
   inputElement.value = ""
 }
+addButton2.onclick = function() {
+  const card = createCard(inputElement2.value)
+  container2.append(card)
+
+  inputElement2.value = ""
+}
+
+// 発展１エンターキー
 
 // inputElement.addEventListener("keypress", enterEvent)
 
@@ -28,6 +39,16 @@ inputElement.onkeydown = function(e) {
     inputElement.value = ""
   }
 }
+inputElement2.onkeydown = function(e) {
+  if (e.keyCode === 13) {
+    const card = createCard(inputElement2.value)
+    container2.append(card)
+
+    inputElement2.value = ""
+  }
+}
+
+// エンターキー終わり
 
 const createCard = function(text) {
   const card = document.createElement("div")

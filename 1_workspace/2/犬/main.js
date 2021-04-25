ff070b69813095d91ddfdca0e2e8fbae66575ad1
taku0708 <78.tk.nghr@gmail.com>
2021-04-25 10:26:38 +0900

    API

--- 1_workspace/2/犬/main.js
+++ 1_workspace/2/犬/main.js
@@ -1,4 +1,5 @@
 const imageElement = document.getElementById("dog-image")
+const imageElement2 = document.getElementById("cat-image")
 
 // 指定したサーバーにデータを取りに行く
 fetch("https://dog.ceo/api/breeds/image/random")
@@ -7,4 +8,12 @@ fetch("https://dog.ceo/api/breeds/image/random")
   })
   .then((data) => {
     imageElement.src = data.message // 画像を表示する
+  })
+
+fetch("https://api.thecatapi.com/v1/images/search")
+  .then((res) => {
+    return res.json() // 結果を json として読み込む
+  })
+  .then((data) => {
+    imageElement2.src = data.message // 画像を表示する
   })

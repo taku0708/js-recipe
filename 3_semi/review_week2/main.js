f39912f9f0ae3311e320873a2014f93ebf0f896c
taku0708 <78.tk.nghr@gmail.com>
2021-05-01 16:57:22 +0900

    semi 3

--- 3_semi/review_week2/main.js
+++ 3_semi/review_week2/main.js
@@ -25,3 +25,70 @@ const createNew = () => {
 }
 
 createNew()
+
+// const toMorseCode = funciton() {
+//     string.prototype.split("")
+//     console.log
+// }
+
+// const str = "00001010101010"
+// const split = str.prototype.split("")
+// console.log(split[3])
+// // const toMorseCode = () = {
+//     if ()
+// }
+
+//
+const toMorseCode = (strNum) => {
+  let MorseCode = ""
+  for (let i = 0; i < strNum.length; i++) {
+    if (strNum[i] === "0") {
+      MorseCode += "."
+    } else {
+      MorseCode += "-"
+    }
+  }
+  return MorseCode
+}
+
+console.log(toMorseCode("01010101000"))
+
+// const addAllNumbers = (numStr) => {
+//   let split = numStr.split("-")
+//   //   let plus = () => {
+//   //     let splitAdd = ""
+//   //     for (let i = 0; i < split.length; i++) {
+//   //       splitAdd += split[i]
+//   //     }
+//   //   }
+//   let splitAdd = ""
+//   let splitTotal = eval(splitAdd)
+
+//   for (let i = 0; i < split.length; i++) {
+//     splitAdd += split[i]
+//   }
+
+//   return splitTotal
+// }
+
+// console.log(addAllNumbers("123-123-45"))
+
+// const addAllNumbers = (numStr) => {
+//   const cuts = numStr.split("-")
+//   let result = 0
+//   for (let i = 0; i < numStr.length; i++) {
+//     result += Number(cuts[i])
+//   }
+//   return result
+// }
+
+const addAllNumbers = function(numString) {
+  const split = numString.split("-")
+  let result = 0
+  for (let i = 0; i < split.length; i++) {
+    result += Number(split[i])
+  }
+  return result
+}
+
+console.log(addAllNumbers("123-122-45"))
